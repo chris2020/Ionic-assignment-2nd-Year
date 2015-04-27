bb0834281b514f338557ef9dc16bce31afd06359
Christy <christymadden@gmail.com>
2015-04-27 15:30:26 +0100

    cleaned up app.js for better readability

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,23 @@
-angular.module('calorific', ['ionic'])
+//storing in variable for better readability
+var christyApp = angular.module('calorific', ['ionic']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+christyApp.run(function($ionicPlatform) 
+{
+    
+  $ionicPlatform.ready(function() 
+  {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) 
+    {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+      
+    if(window.StatusBar)
+    {
       StatusBar.styleDefault();
     }
+      
   });
-})
+    
+});
